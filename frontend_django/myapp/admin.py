86b97e52cc55c3a2299a95ab0210858061801f14
Tulsi027 <tulsidesai027@gmail.com>
2025-10-17 22:36:09 +0530

    Removed Student model

--- frontend_django/myapp/admin.py
+++ frontend_django/myapp/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from .models import Student
+#from django.contrib import admin
+#from .models import Student
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "email"]
+#@admin.register(Student)
+#class StudentAdmin(admin.ModelAdmin):
+#    list_display = ["id", "name", "email"]
